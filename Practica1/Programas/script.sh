db44301e7d9a78cbc6c776ff6a78cec58b6586d5
Enrike Ramos <39656338+brokenerk@users.noreply.github.com>
2018-09-02 20:01:29 -0500

    Shell en script

--- Practica1/Programas/script.sh
+++ Practica1/Programas/script.sh
@@ -60,3 +60,25 @@ gcc Seleccion.c tiempo.o -o Seleccion
 ./Seleccion 9000000 <numeros10millones.txt >>salida.txt
 ./Seleccion 10000000 <numeros10millones.txt >>salida.txt
 
+gcc Shell.c tiempo.o -o Shell
+./Shell 100 <numeros10millones.txt >>salida.txt
+./Shell 1000 <numeros10millones.txt >>salida.txt
+./Shell 5000 <numeros10millones.txt >>salida.txt
+./Shell 10000 <numeros10millones.txt >>salida.txt
+./Shell 50000 <numeros10millones.txt >>salida.txt
+./Shell 100000 <numeros10millones.txt >>salida.txt
+./Shell 200000 <numeros10millones.txt >>salida.txt
+./Shell 400000 <numeros10millones.txt >>salida.txt
+./Shell 600000 <numeros10millones.txt >>salida.txt
+./Shell 800000 <numeros10millones.txt >>salida.txt
+./Shell 1000000 <numeros10millones.txt >>salida.txt
+./Shell 2000000 <numeros10millones.txt >>salida.txt
+./Shell 3000000 <numeros10millones.txt >>salida.txt
+./Shell 4000000 <numeros10millones.txt >>salida.txt
+./Shell 5000000 <numeros10millones.txt >>salida.txt
+./Shell 6000000 <numeros10millones.txt >>salida.txt
+./Shell 7000000 <numeros10millones.txt >>salida.txt
+./Shell 8000000 <numeros10millones.txt >>salida.txt
+./Shell 9000000 <numeros10millones.txt >>salida.txt
+./Shell 10000000 <numeros10millones.txt >>salida.txt
+
