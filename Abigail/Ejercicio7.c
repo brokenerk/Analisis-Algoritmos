151707cdb575a49046ac9ad263bc0c2878855f7c
Abii Snn <33185677+abiisnn@users.noreply.github.com>
2018-09-12 20:44:29 -0500

    Graficas

--- Abigail/Ejercicio7.c
+++ Abigail/Ejercicio7.c
@@ -2,25 +2,24 @@
 
 int main()
 {
-	int i, j, n;
+	int i, j, n=10;
 	int contador = 0;
-	
-	while(n != -1)
+	int Funcion;
+
+	for(j = n; j > 1; j /=2 )
 	{
-		scanf("%d", &n);
-		for(j = n; j > 1; j /=2 )
+		if(j < (n/2))
 		{
-			if(j < (n/2))
+			for(i = 0; i < n; i += 2)
 			{
-				for(i = 0; i < n; i += 2)
-				{
-					printf("\"Algoritmos\"\n");
-					contador++;
-				}
+				printf("\"Algoritmos\"\n");
+				contador++;
 			}
-		}	
-		printf(" Contador: %d \n", contador);
-		contador = 0;
-	}
-	
+		}
+	}	
+	Funcion = (int)((n/2)*((log(n/2)/log(2))-1));
+
+	printf(" Funcion: %d \n", Funcion);
+	printf(" Contador: %d \n", contador);
+	contador = 0;	
 }
