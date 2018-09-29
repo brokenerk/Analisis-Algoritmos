98d7d5280d9744d38b4a1d03d2a681c4b2b24d3d
Abii Snn <33185677+abiisnn@users.noreply.github.com>
2018-09-28 21:30:49 -0500

    FataFalta busqueda

--- Practica2/Codigo/Arbol/ABB.c
+++ Practica2/Codigo/Arbol/ABB.c
@@ -4,7 +4,7 @@
 //	Parra Garcilazo Cinthya Dolores
 // 	Ramos Diaz Enrique
 // ****************************************************************
-//	Practica 1: Analisis de algoritmos de ordenamiento numerico
+//	Practica 2: Análisis temporal y notación de orden (Algoritmos de búsqueda)
 //	Compilación:
 //	gcc tiempo.c -c
 //	gcc ABB.c tiempo.o -o ABB
@@ -18,7 +18,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "Arbin.h"
-#include "tiempo.h"
 
 //	***************************************************************
 //						Insertar
@@ -77,63 +76,53 @@ void GuardarRecorridoInOrden(Arbin *a, int A[], int n)
 	free(pila);
 }
 
-//	***************************************************************
-//						OrdenaConArbolBinario
-//	***************************************************************
-//	Descripción: Ordena un arreglo de numeros utilizando diccionarios binarios
-//	Recibe: Un arreglo de enteros y el tamaño del arreglo
-//	Devuelve: Nada, pero ordena los elementos del arreglo
-//	***************************************************************
-void OrdenaConArbolBinario(int A[], int n)
-{
-	double utime0, stime0, wtime0,utime1, stime1, wtime1; //Variables para medición de tiempos
-	uswtime(&utime0, &stime0, &wtime0);
-
-	Arbin ArbolBinBusqueda;
-	consA(&ArbolBinBusqueda); //Asignamos el valor NULL al apuntador del ABB
-	int i, j = 0;
-
-	for(i = 0; i < n; i++)
-	{
-		Insertar(&ArbolBinBusqueda, A[i]);
-	}
-
-	GuardarRecorridoInOrden(&ArbolBinBusqueda, A, n);
-
-	destruir(&ArbolBinBusqueda);
-
-	uswtime(&utime1, &stime1, &wtime1);
-
-	//Cálculo del tiempo de ejecución del programa
-	printf("\nArbol de Busqueda Binario\n");
-	printf("real (Tiempo total)  %.35f s\n",  wtime1 - wtime0);
-	printf("user (Tiempo de procesamiento en CPU) %.35f s\n",  utime1 - utime0);
-	printf("sys (Tiempo en acciónes de E/S)  %.35f s\n",  stime1 - stime0);
-	printf("CPU/Wall   %.35f %% \n",100.0 * (utime1 - utime0 + stime1 - stime0) / (wtime1 - wtime0));
-	printf("\n");
-
-	/*//Para imprimir los numeros de arreglo y verificar el algoritmo
-	for(i=0; i<n; i++){
-		printf("%d, ", A[i]);
-	}*/
-}
 
 int main(int argc, char *argv[])
 {
 	//Obtenemos n como parametro del main y creamos una arreglo dinamico
-	int n = atoi(argv[1]);
+	int n = 10; i = 0, j = 0;
+	int s; // Variable bandera, indica si se encuentra un numero
+	float suma = 0, promedio = 0;
 	int *arreglo = (int*)calloc(n,sizeof(int));
+	int datos[20] = {322486, 14700764, 3128036, 6337399, 61396,
+					10393545, 2147445644, 1295390003, 450057883, 
+					187645041, 1980098116, 152503, 5000, 1493283650, 
+					214826, 1843349527, 1360839354, 2109248666 , 
+					2147470852, 0};
 
-	printf("n = %d\n", n);
 	//Con este for vamos agregando los n valores del txt al arreglo
-	for(int i = 0; i < n; i++)
-	{
+	for(i = 0; i < n; i++)
 		fscanf(stdin, "%d", &arreglo[i]);
+	printf("\nArbol Binario de Busqueda = %d", n);
+
+	// Creamos un arbol
+	Arbin ArbolBinBusqueda;
+	consA(&ArbolBinBusqueda); //Asignamos el valor NULL al apuntador del ABB
+	
+	// Insertamos los valores del txt en el arbol
+	for(i = 0; i < n; i++)
+	{
+		Insertar(&ArbolBinBusqueda, arreglo[i]);
 	}
+	printf("Se inserto bien :3\n");
 
-	OrdenaConArbolBinario(arreglo, n);
+	//Con este for vamos buscando cada numero en el arreglo
+/*	for(j = 0; j < 20; j++)
+	{
+
+		s = ABB(&ArbolBinBusqueda, arreglo, n, datos[j]);
+
+		if(j == 17)// para 2109248666
+		{
+			if(s != -1)
+				printf("\n\n%d SI : %d ", datos[j], s);
+			else
+				printf("\n\n%d NO : --- ", datos[j]);
+		}
+	}
+*/	
 	printf("------------------------------------\n");
-
+	exit(0);
 	return 0;
 }
 
