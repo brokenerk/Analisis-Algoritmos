e5cb8e82e3bd771a0e2fd2b01ff8efa1e4b8db37
Enrike Ramos <39656338+brokenerk@users.noreply.github.com>
2018-11-30 23:36:11 -0600

    ej10

---
+++ Enrike/Ejercicio10/powerstr.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int max(int a, int b){
+    if(a > b) return a;
+    else return b;
+}
+ 
+char cadena[1000015];
+int main(){
+    while(scanf("%s", cadena)) {
+        if(strcmp(cadena, ".") == 0) break;
+ 
+        int longitud = strlen(cadena);
+        int power = 1;
+ 
+        for(int i = 2; i <= longitud; i++){
+            if(longitud % i == 0){
+                int L = longitud / i;
+                int empata = 1;
+                for(int j = L; j < longitud; j++){
+                    if (cadena[j] != cadena[j % L]){ 
+                        empata = 0; 
+                        break;
+                    }
+                }
+                if(empata){ 
+                    power = max(power, i);
+                }
+            }
+        }
+        printf ("%d\n", power);
+    }
+    return 0;
+}
