c36d2278844d32d35322e551f175a669d992bcb9
Enrike Ramos <39656338+brokenerk@users.noreply.github.com>
2018-12-04 19:01:59 -0600

    fin ej10

--- Enrike/Ejercicio10/powerstr.c
+++ Enrike/Ejercicio10/powerstr.c
@@ -2,31 +2,32 @@
 #include <string.h>
 #include <stdlib.h>
 
-int max(int a, int b){
-    if(a > b) return a;
-    else return b;
-}
- 
-char *cadena = (char*)calloc(10000000, sizeof(char*));
+char *cadena = (char*)calloc(10000000*5, sizeof(char*));
 int main(){
-    while(scanf("%s", cadena)) {
+    while(scanf("%s", cadena)){
+        /*Leemos y evaluamos n cadenas hasta hallar un punto*/
         if(strcmp(cadena, ".") == 0) break;
- 
         int longitud = strlen(cadena);
+        //Es claro que toda cadena esta elevada a la 1 por defecto, asumimos que a es la misma cadena s
         int power = 1;
- 
+        /*Iniciamos desde 2 porque toda cadena esta elevada a la 1 por defecto*/
         for(int i = 2; i <= longitud; i++){
+            /*Revisamos divisibilidad para nuestra n propuesta*/
             if(longitud % i == 0){
                 int L = longitud / i;
-                int empata = 1;
+                //Asumimos que la subcadena a siempre empata con las anteriores
+                int empata = 1; //Bandera
                 for(int j = L; j < longitud; j++){
+                    /*Algoritmo KMP*/
                     if (cadena[j] != cadena[j % L]){ 
+                        /*En cuanto la subcadena no coincida con a, acabamos. Probamos con otra n*/
                         empata = 0; 
                         break;
                     }
                 }
                 if(empata){ 
-                    power = max(power, i);
+                    /*Vamos obteniendo el periodo o potencia de la subcadena a si sí empató*/
+                    power = i;
                 }
             }
         }
--- Enrike/Ejercicio10/shift.c
+++ Enrike/Ejercicio10/shift.c
@@ -1,10 +1,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-int max(int a, int b){
-    if(a > b) return a;
-    else return b;
-}
 
 int main(){
 	int n;
@@ -13,29 +9,29 @@ int main(){
 	char *B = (char*)calloc(n+1, sizeof(char*));
 	scanf("%s %s", A, B);
 	//Prefijo: subcadena a la izquierda
-	int prefix = 0;
-	int shift = 0;
-	int i = 0;
+	int prefix = 0, shift = 0;
+	int i = 0, cont=0;
 	while(i < n){
 		//Revisar el primer caracter del prefijo de A
 		if(B[i] == A[0]){
-			//Revisar los siguientes caracteres del prefijo, si existen
+			//Revisar los siguientes caracteres del prefijo, si existen (KMP)
 			if(B[(i + prefix) % n] == A[prefix]){
-				int j = 0;
-				int aux = 0;
-				//Hacemos las shift operations
+				int j = 0, aux = 0;
+				//Hacemos las shift operations mientras el prefijo coincida
 				while(B[i + j] == A[j]){
 					aux++;
 					j = (j + 1) % n;
+					cont++;
 				}
-				/*Revisamos si las shift operations son mayores al valor anterior
-				si lo son, lo sustituimos. Obtenemos el máximo de ambos*/
-				prefix = max(aux, prefix);
-				shift = max(i, shift);
+				/*Si la longitud del nuevo prefijo encontrado es mayor, actualizamos valores*/
+				if(aux > prefix){
+					prefix = aux;
+					shift = i;
+				}
 			}
 		}
 		i++;
 	}
-	printf("%d\n", shift);
+	printf("%d %d\n", shift, cont);
 	return 0;
 }
