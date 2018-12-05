77e276309629ed3d11317091a3374cea8823d989
Enrike Ramos <39656338+brokenerk@users.noreply.github.com>
2018-12-04 23:25:53 -0600

    reporte ej10

--- Enrike/Ejercicio10/powerstr.c
+++ Enrike/Ejercicio10/powerstr.c
@@ -10,7 +10,7 @@ int main(){
         int longitud = strlen(cadena);
         //Es claro que toda cadena esta elevada a la 1 por defecto, asumimos que a es la misma cadena s
         int power = 1;
-        /*Iniciamos desde 2 porque toda cadena esta elevada a la 1 por defecto*/
+        /*Iniciamos desde 2*/
         for(int i = 2; i <= longitud; i++){
             /*Revisamos divisibilidad para nuestra n propuesta*/
             if(longitud % i == 0){
@@ -21,8 +21,7 @@ int main(){
                     /*Algoritmo KMP*/
                     if (cadena[j] != cadena[j % L]){ 
                         /*En cuanto la subcadena no coincida con a, acabamos. Probamos con otra n*/
-                        empata = 0; 
-                        break;
+                        empata = 0; break;
                     }
                 }
                 if(empata){ 
